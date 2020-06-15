c6eb1149d8ea305b5eec2a47b29b81926d766ea4
Connorrichter4 <Connorrichter4@gmail.com>
2020-06-14 21:47:51 -0500

    updated movie model

--- moviebuddy/models.py
+++ moviebuddy/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Movie(models.Model):
     title = models.CharField(max_length=100)
-    image_url = models.URLField()
+    image_url = models.URLField(default='https://lh6.googleusercontent.com/proxy/hIgFSMyx4VsuoQh8h-ZfI3IiK9uFSLZ7pG67H_1RwEBDEPiWX-odcJ0PkWriAPeqwKyC6n-12UTrNmQF2ul9DAjwKMljG3zSCCTDoTVDPexFHV9l_JD5WMbmpnUJqWLqYA=s0-d', editable=False)
     trailer_url = models.URLField(blank=True, default='')
     description = models.TextField()
     year_released = models.CharField(max_length=100)
